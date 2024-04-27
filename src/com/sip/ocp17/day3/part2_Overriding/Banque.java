b08298d2b20bee7ad77331e9cbabd9c4336ba6fb
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-04-27 16:03:48 +0100

    correction exo

--- src/com/sip/ocp17/day3/part2_Overriding/Banque.java
+++ src/com/sip/ocp17/day3/part2_Overriding/Banque.java
@@ -7,6 +7,10 @@ public class Banque {
 		CompteBancaire cb2 = new CompteBancaire(123,500);
 		
 		System.out.println(cb1.equals(cb2));
+		String ch1 = new String("ocp");
+		String ch2 = new String("ocp");
+		
+		System.out.println(ch1.equals(ch2));
 
 	}
 
--- src/com/sip/ocp17/day3/part2_Overriding/CompteBancaire.java
+++ src/com/sip/ocp17/day3/part2_Overriding/CompteBancaire.java
@@ -3,10 +3,22 @@ package com.sip.ocp17.day3.part2_Overriding;
 public class CompteBancaire {
 	int rib;
 	double solde;
-	
+
 	public CompteBancaire(int rib, double solde) {
 		this.rib = rib;
 		this.solde = solde;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof CompteBancaire) {
+			CompteBancaire compte = (CompteBancaire) obj;
+			if (this.rib == compte.rib && this.solde == compte.solde) {
+				return true;
+			}
+
+		}
+		return false;
 	}
 
 }
